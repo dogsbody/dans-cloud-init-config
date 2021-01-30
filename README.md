01590d4b27aa163bd3708c38b0592597f4c709c9
Dan Benton <dan@dogsbody.com>
2021-01-30 13:05:09 +0000

    Initial commit now we have something useful

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# dans-cloud-init-config
-My entirely personal cloud-init config for test, dev and play
+# Dan's cloud-init config generator
+
+My entirely personal cloud-init config generator for test, dev and play.
+
+## Installation
+
+    git clone --recurse-submodules https://github.com/dogsbody/MainProject
+
+or 
+
+    git clone https://github.com/dogsbody/MainProject
+    git submodule init
+    git submodule update
+
+
+## Usage
+
+Just run `generate-userdata.sh` an answer the prompts
+
+
+## ToDo
+* Set a hostname (and instance id?)
+* How do we know when the final reboot has happened?
+* Setup e-mail?
+* Turn off secure boot in Ubuntu?
+
+
---
+++ generate-userdata.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+#
+# Description:  A script to create valid user-data for cloud-init
+#
+# Usage:  $0
+#
+# Configuration:  Source files can be found in the `src` directory
+#     src/cloudconfig  A file used as a template for the cloud-config
+#     src/userscript   A script to be run at the end of the first bootup 
+#     src/root/        Files that should be placed on the system in the appropriate directories
+#
+# Notes:  Currently all files are created on the system as root with permissions 644.
+#
+
+set -e
+set -u
+
+# Basic check that we have the tools that all systems should have (uses set -e)
+# echo, cd, pwd are all bash built-in's
+hash cp
+hash sed
+hash find
+hash base64
+hash dirname
+
+# Set some variables
+printf -v DATE '%(%Y%m%d%H%M%S)T' -1
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WRITEMIME="$SCRIPTDIR/cloud-utils/bin/write-mime-multipart"
+CLDLOCALD="$SCRIPTDIR/cloud-utils/bin/cloud-localds"
+OURCLOUDCONFIG="$SCRIPTDIR/$DATE-cloudconfig"
+OURUSERDATARAW="$SCRIPTDIR/$DATE-userdata.raw"
+OURUSERDATAIMG="$SCRIPTDIR/$DATE-userdata.img"
+
+# Check we have our submodule installed
+if [[ ! -x ${WRITEMIME} || ! -x ${CLDLOCALD} ]];then
+  echo "Error: Can't find the scripts we need in the cloud-utils sub-repo"
+  echo "  You probably just need to initialise and update the submodule..."
+  echo "    git submodule init -C ${SCRIPTDIR}"
+  echo "    git submodule update -C ${SCRIPTDIR}"
+  exit
+fi
+
+# Get a password
+echo 
+echo "Please enter a password that will be used for the root password..."
+read -sp 'Password: ' PASSWORD
+echo -e "\n"
+
+# Create our cloudconfig from the template and set the password
+cp $SCRIPTDIR/src/cloudconfig $OURCLOUDCONFIG
+sed -i -r "s/^password: trustno1$/password: $PASSWORD/" $OURCLOUDCONFIG
+
+# Encode and add the files into the cloudconfig
+echo "write_files:" >> $OURCLOUDCONFIG
+for FILE in $(find "$SCRIPTDIR/src/root" -type f -print); do
+  echo "- path: ${FILE#$SCRIPTDIR/src/root}"      >> $OURCLOUDCONFIG
+  echo "  content: $(base64 --wrap=0 "${FILE}")"  >> $OURCLOUDCONFIG
+  echo "  encoding: base64"                       >> $OURCLOUDCONFIG
+  # Leaving these options here not 'cos they are needed but as prompts for improvement in the future 
+  echo "  owner: root:root"   >> $OURCLOUDCONFIG
+  echo "  permissions: 0644"  >> $OURCLOUDCONFIG
+  echo "  append: false"      >> $OURCLOUDCONFIG
+done
+echo "Cloudconfig: $OURCLOUDCONFIG"
+
+# Create our multipart file
+$WRITEMIME $SCRIPTDIR/src/userscript:text/x-shellscript $OURCLOUDCONFIG --output=$OURUSERDATARAW
+echo "Raw Userdata: $OURUSERDATARAW"
+
+# Create our userdata volume
+$CLDLOCALD $OURUSERDATAIMG $OURUSERDATARAW
+echo "Userdata Disk Image: $OURUSERDATAIMG"
+
+
